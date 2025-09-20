7e5469d99666013f3b8330a8f751459120067e3a
anytls <anytls>
2025-09-20 22:06:17 +0900

    chore(session): remove useless retries

--- proxy/session/client.go
+++ proxy/session/client.go
@@ -68,20 +68,17 @@ func (c *Client) CreateStream(ctx context.Context) (net.Conn, error) {
 	var stream *Stream
 	var err error
 
-	for i := 0; i < 3; i++ {
-		session, err = c.findSession(ctx)
-		if session == nil {
-			return nil, fmt.Errorf("failed to create session: %w", err)
-		}
-		stream, err = session.OpenStream()
-		if err != nil {
-			session.Close()
-			continue
-		}
-		break
+	session = c.getIdleSession()
+	if session == nil {
+		session, err = c.createSession(ctx)
 	}
-	if session == nil || stream == nil {
-		return nil, fmt.Errorf("too many closed session: %w", err)
+	if session == nil {
+		return nil, fmt.Errorf("failed to create session: %w", err)
+	}
+	stream, err = session.OpenStream()
+	if err != nil {
+		session.Close()
+		return nil, fmt.Errorf("failed to create stream: %w", err)
 	}
 
 	stream.dieHook = func() {
@@ -102,9 +99,7 @@ func (c *Client) CreateStream(ctx context.Context) (net.Conn, error) {
 	return stream, nil
 }
 
-func (c *Client) findSession(ctx context.Context) (*Session, error) {
-	var idle *Session
-
+func (c *Client) getIdleSession() (idle *Session) {
 	c.idleSessionLock.Lock()
 	if !c.idleSession.IsEmpty() {
 		it := c.idleSession.Iterate()
@@ -112,12 +107,7 @@ func (c *Client) findSession(ctx context.Context) (*Session, error) {
 		c.idleSession.Remove(it.Key())
 	}
 	c.idleSessionLock.Unlock()
-
-	if idle == nil {
-		s, err := c.createSession(ctx)
-		return s, err
-	}
-	return idle, nil
+	return
 }
 
 func (c *Client) createSession(ctx context.Context) (*Session, error) {
